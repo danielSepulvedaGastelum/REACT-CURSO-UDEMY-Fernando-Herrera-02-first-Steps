fdd4a1605954f419e81cd5c13284a07e7e54ab5e
Daniel Sepulveda <daniel.sepu@gmail.com>
2026-08-12 20:47:24 -0700

    Clase 64 - Finalizando Sección de Testing

--- src/FirstStepsApp.test.tsx
+++ src/FirstStepsApp.test.tsx
@@ -1,15 +1,15 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, test, vi } from "vitest";
+import { afterEach, describe, expect, test, vi } from "vitest";
 import { FirstStepsApp } from "./FirstStepsApp";
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
-const mockItemCounter = vi.fn((props: unknown) => {
+const mockItemCounter = vi.fn((_props: unknown) => {
     return <div data-testid="itemCounter" />;
 });
 
 
 vi.mock('./shopping-cart/ItemCounter', () => ({
-    ItenCounter: (props: unknown) => mockItemCounter(props),
+    ItemCounter: (props: unknown) => mockItemCounter(props),
 }));
 
 // vi.mock('./shopping-cart/ItemCounter', () => ({
@@ -28,6 +28,11 @@ vi.mock('./shopping-cart/ItemCounter', () => ({
 
 describe('FirstStepsApp', () => {
 
+    //Esta función se ejecuta despues de cada test();
+    afterEach( () =>{
+        vi.clearAllMocks();
+    });
+
     test('should match snapshop', () => {
         const { container } = render(<FirstStepsApp />);
         expect(container).toMatchSnapshot();
@@ -36,13 +41,32 @@ describe('FirstStepsApp', () => {
 
     test('should render the correct number of ItemCounter compoment', () => {
         render(<FirstStepsApp />);
-
         const itemCounters = screen.getAllByTestId('itemCounter');
-
         expect(itemCounters.length).toBe(4);
-
-
-        screen.debug();
+        // screen.debug();
     })
+
+    test('should render ItemCounter with correct props', () => {
+        render(<FirstStepsApp/>);
+        // Para comprobar que sean 4 elementos hijos
+        expect(mockItemCounter).toHaveBeenCalledTimes(4);
+        // para confirmar que de cada elemento hijo las props seas las que deben de ser
+        expect(mockItemCounter).toHaveBeenCalledWith({ 
+            name: 'Nintnedo Switch 2', 
+            quantity: 1 
+        });
+        expect(mockItemCounter).toHaveBeenCalledWith({ 
+            name: 'Pro controller', 
+            quantity: 2 
+        });
+        expect(mockItemCounter).toHaveBeenCalledWith({ 
+            name: 'Super Smash Bross', 
+            quantity: 5 
+        });
+        expect(mockItemCounter).toHaveBeenCalledWith({ 
+            name: 'Super Mario Wonder', 
+            quantity: 3 
+        });
+    });
 
 })
